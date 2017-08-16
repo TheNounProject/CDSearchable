d60632d714a00c8281d3770632b6c696e839da24
Wes Byrne <wesburn@me.com>
2017-08-15 18:30:59 -0700

    Initial Commit

---
+++ Searchable/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Searchable
+//
+//  Created by Wesley Byrne on 8/15/17.
+//  Copyright © 2017 nounproject. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
